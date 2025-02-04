dadc531f0cdb6b48a2ab4152fd68051e5b07bbca
William Tichenor <williamtichenor@tamu.edu>
2025-02-04 16:40:50 -0600

    basic completion

---
+++ RISRandomSample.py
@@ -0,0 +1,23 @@
+import sys
+import random
+import rispy
+
+
+fpath = sys.argv[1]
+with open(fpath, 'r', encoding="utf-8-sig") as dataFile:
+    data = rispy.load(dataFile)
+    outputContent = []
+    totalNum = len(data)
+    samples = int(sys.argv[2])
+    if (samples>totalNum):
+        samples = totalNum
+    print("Num Samples: "+str(samples))
+    print("Total Entries: "+str(totalNum))
+    for i in range(samples):
+        randIndex = random.randrange(0,totalNum-i)
+        outputContent.append(data[randIndex])
+        data.pop(randIndex)
+    print(str(len(outputContent))+" Entries")
+    outpath = 'output/output.ris'
+    with open(outpath, 'w', encoding="utf-8-sig") as dataFile:
+        rispy.dump(outputContent, dataFile)
